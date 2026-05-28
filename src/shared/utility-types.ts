998087cb060d4e5994a836c1cd98b066b19173d9
theo <edem.teo@gmail.com>
2026-05-28 17:05:59 +0300

    feat: Utility types

---
+++ src/shared/utility-types.ts
@@ -0,0 +1,13 @@
+export type Nullable<T> = T | null;
+export type Optional<T> = T | undefined;
+export type Maybe<T> = T | null | undefined;
+export type Primitive = string | number | boolean | null | undefined;
+export type DeepPartial<T> = {
+  [P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
+};
+export type DeepRequired<T> = {
+  [P in keyof T]-?: T[P] extends object ? DeepRequired<T[P]> : T[P];
+};
+export type DeepReadonly<T> = {
+  readonly [P in keyof T]: T[P] extends object ? DeepReadonly<T[P]> : T[P];
+};
